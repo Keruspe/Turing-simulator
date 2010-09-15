676f68e5181cee9931b445c29b94fad697565577
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-09-15 18:21:11 +0200

    secure headers

--- data.h
+++ data.h
@@ -1,6 +1,10 @@
+#ifndef __DATA_H__
+#define __DATA_H__
+
 typedef struct _Data {
 	int dummy;
 } Data;
 
 Data * newData(char *);
 
+#endif /* __DATA_H__ */
--- machine.h
+++ machine.h
@@ -1,5 +1,10 @@
+#ifndef __MACHINE_H__
+#define __MACHINE_H__
+
 typedef struct _Machine {
 	int dummy;
 } Machine;
 
 Machine * newMachine(char *);
+
+#endif /* __MACHINE_H__ */
