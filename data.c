69052b140a90148818858494ab7dca22f6c2f13a
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2010-10-19 10:54:53 +0200

    Comment Data and Machine

--- data.c
+++ data.c
@@ -3,6 +3,7 @@
 Data *
 _newData()
 {
+	/* Basic allocation and initialisation stuff to get a ready base Data */
 	Data * data = (Data *) malloc(sizeof(Data));
 	data->data = (Letter *) malloc(BASE_DATA_LENGTH * sizeof(Letter));
 	data->data_length = 0;
@@ -18,12 +19,15 @@ newData()
 	char dataFilename[MAX_FILENAME_LENGTH];
 
 	printf("Where is the file containing the data for your machine ?\n");
+	/* Read the name of the file in which the Data are stored, don't fail if we cannot, just ask for another one */
 	while (!scanf("%s", dataFilename) || (dataFile = fopen(dataFilename, "r")) == NULL)
 		printf("Failed to read file: %s, please enter a new one.\n", dataFilename);
 
-	Data * data = _newData();
+	Data * data = _newData(); /* Allocate memory */
+	/* Extract Data from dataFile */
 	_extractData(dataFile, &(data->data), &(data->data_length));
 
+	/* Close the dataFile */
 	fclose(dataFile);
 	return data;
 }
@@ -31,9 +35,11 @@ newData()
 void
 freeData(Data * data)
 {
-	int i;
+	/* Free the memory used by a Data set */
+	int i; /* Counter */
 	for (i = 0 ; i < data->data_length ; ++i)
-		free(data->data[i]);
+		free(data->data[i]); /* Free each Letter from data */
+	/* Free remaining stuff */
 	free(data->data);
 	free(data->extra_data);
 	free(data);
--- machine.c
+++ machine.c
@@ -3,6 +3,7 @@
 Machine *
 _newMachine()
 {
+	/* Basic allocation and initialisation stuff to get a ready base Machine */
 	Machine * machine = (Machine *) malloc(sizeof(Machine));
 	machine->alphabet = (Letter *) malloc(BASE_ALPHABET_LENGTH * sizeof(Letter));
 	machine->alphabet_length = 0;
@@ -17,39 +18,43 @@ _newMachine()
 void
 _readAlphabet(Machine * machine, FILE * machineFile)
 {
+	/* Read the alphabet recognized by the Machine from machineFile */
 	_extractData(machineFile, &(machine->alphabet), &(machine->alphabet_length));
 }
 
 void
 _readStates(Machine * machine, FILE * machineFile)
 {
+	/* Read the states in which the Machine can be from machineFile */
 	_extractData(machineFile, &(machine->states), &(machine->states_length));
 }
 
 void
 _readTransitions(Machine * machine, FILE * machineFile)
 {
-	Element element;
-	while (! ((element = _readElement(machineFile)).endOfElements))
+	/* Read the available transitions */
+	Element element; /* Will be use to store read data */
+	while (! ((element = _readElement(machineFile)).endOfElements)) /* Keep reading while there are things to read (we did not meet '#') */
 	{
-		machine->transitions[machine->transitions_length].start_state = element.element;
-		machine->transitions[machine->transitions_length].cond = _readElement(machineFile).element;
-		machine->transitions[machine->transitions_length].subst = _readElement(machineFile).element;
-		machine->transitions[machine->transitions_length].next_state = _readElement(machineFile).element;
-		element = _readElement(machineFile);
-		Move move = element.element[0];
-		free(element.element);
+		machine->transitions[machine->transitions_length].start_state = element.element; /* The first element we just read is the start state */
+		machine->transitions[machine->transitions_length].cond = _readElement(machineFile).element; /* Then read the conditionnal value */
+		machine->transitions[machine->transitions_length].subst = _readElement(machineFile).element; /* Then the substitution value */
+		machine->transitions[machine->transitions_length].next_state = _readElement(machineFile).element; /* Continue with the next state */
+		element = _readElement(machineFile); /* Now read what will be the direction of the next move */
+		Move move = element.element[0]; /* We only want the first char of it */
+		free(element.element); /* So free this array not to have any leak */
 		if (move != 'R' && move != 'L')
 		{
+			/* Move was not 'R' or 'L', we don't know any other, abort */
 			printf("Bad move, only 'R' or 'L' are allowed, exiting...\n");
 			++machine->transitions_length;
 			freeMachine(machine);
 			exit(EXIT_FAILURE);
 		}
-		machine->transitions[machine->transitions_length++].move = move;
-		if (element.endOfElements)
-			break;
-		if ((machine->transitions_length % BASE_TRANSITIONS_LENGTH) == 0)
+		machine->transitions[machine->transitions_length++].move = move; /* Store the move and increase the number of transitions available */
+		if (element.endOfElements) /* If we reached a '#', just exit this function */
+			return;
+		if ((machine->transitions_length % BASE_TRANSITIONS_LENGTH) == 0) /* When the Array is full, increase its size */
 			machine->transitions = realloc(machine->transitions, (machine->transitions_length + BASE_TRANSITIONS_LENGTH) * sizeof(Transition));
 	}
 }
@@ -57,6 +62,7 @@ _readTransitions(Machine * machine, FILE * machineFile)
 void
 _readStartAndEndPoints(Machine * machine, FILE * machineFile)
 {
+	/* Read the initial state, and the the final state of the Machine */
 	machine->initial_state = _readElement(machineFile).element;
 	machine->final_state = _readElement(machineFile).element;
 }
@@ -68,18 +74,21 @@ newMachine()
 	char machineFilename[MAX_FILENAME_LENGTH];
 
 	printf("Where is the file describing your turing machine ?\n");
+	/* Read the name of the file in which the Machine is described, fail and exit if we cannot load it */
 	if (!scanf("%s", machineFilename) || (machineFile = fopen(machineFilename, "r")) == NULL)
 	{
 		printf("Failed to read file: %s, exiting...\n", machineFilename);
 		exit(EXIT_FAILURE);
 	}
 
-	Machine * machine = _newMachine();
+	Machine * machine = _newMachine(); /* Allocate memory */
+	/* Read machine stuff from machineFile */
 	_readAlphabet(machine,machineFile);
 	_readStates(machine, machineFile);
 	_readTransitions(machine, machineFile);
 	_readStartAndEndPoints(machine, machineFile);
 
+	/* Close the machineFile */
 	fclose(machineFile);
 	return machine;
 }
@@ -87,6 +96,7 @@ newMachine()
 void
 freeTransition(Transition transition)
 {
+	/* Free the memory used by a Transition */
 	free(transition.start_state);
 	free(transition.cond);
 	free(transition.subst);
@@ -96,19 +106,21 @@ freeTransition(Transition transition)
 void
 freeMachine(Machine * machine)
 {
-	int i;
+	/* Free the memory used by a Machine */
+	int i; /* Will be a basic counter */
 	for (i = 0 ; i < machine->alphabet_length ; ++i)
-		free(machine->alphabet[i]);
+		free(machine->alphabet[i]); /* Free each Letter in the alphabet */
 	for (i = 0 ; i < machine->states_length ; ++i)
-		free(machine->states[i]);
+		free(machine->states[i]); /* Free each possible State */
 	for (i = 0 ; i < machine->transitions_length ; ++i)
-		freeTransition(machine->transitions[i]);
+		freeTransition(machine->transitions[i]); /* Free each available Transition */
+	/* Free remaining stuff */
 	free(machine->initial_state);
 	free(machine->final_state);
 	free(machine->alphabet);
 	free(machine->states);
 	free(machine->transitions);
-	if (machine->data)
+	if (machine->data) /* If the machine had Data, free it */
 		freeData(machine->data);
 	free(machine);
 }
@@ -116,10 +128,10 @@ freeMachine(Machine * machine)
 void
 reloadData(Machine * machine)
 {
-	if (machine->data)
+	if (machine->data) /* If the machine had Data, free it */
 		freeData(machine->data);
 
+	/* Then read Data */
 	machine->data = newData();
 	clearBuffer(); /* Clear buffer */
 }
-
